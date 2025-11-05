faf59dd1fdd3ec7e70e2bb23937c1fd1a2921398
kIwwwww1 <danil29070@gmail.com>
2025-11-05 21:37:00 +0300

    Создание функции для добавления пользователя в базу

--- commands/main_commands.py
+++ commands/main_commands.py
@@ -1,7 +1,9 @@
+from sqlalchemy import select
 from aiogram import Router, F, types
 from aiogram.filters import CommandStart, Command
 from os import getenv
 from dotenv import load_dotenv
+from database_config.database_setting import User, create_session
 
 load_dotenv()
 
@@ -10,5 +12,13 @@ CHAT_ID = getenv('CHAT_ID')
 user_router = Router()
 
 @user_router.message(CommandStart())
-async def start_command(message: types.Message) -> None:
-    await message.answer('Привет')
+async def start_command(message: types.Message):
+    user_id = message.from_user.id
+    name = message.from_user.username
+    async with create_session() as session:
+        user = (await session.execute(select(User).where(User.id == user_id))).scalar_one_or_none()
+        if not user:
+            session.add(User(id=user_id, user_name=name))
+            await message.answer(f'Привет {name}')
+        else:
+            await message.answer(f'С возвращением, {name}!')
