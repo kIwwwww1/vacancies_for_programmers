cc38a3440d48db340fd8c94606c34baa13b90317
kIwwwww1 <danil29070@gmail.com>
2025-11-06 17:39:49 +0300

    1.0v Создание вакансии

--- commands/create_vacancy.py
+++ commands/create_vacancy.py
@@ -8,25 +8,131 @@ vacancy_router = Router()
 
 class CreateNote(StatesGroup):
     await_title = State()
+    await_type_of_employment = State()
+    await_user_level = State()
     await_location = State()
+    await_responsibilities = State()
+    await_requirements = State()
+    await_mb_plus = State()
+    await_additionally = State()
+    await_contacts = State()
+    await_stack = State()
+
 
 @vacancy_router.message(F.text == ReplyTextCommand.CREATE_VACANCY)
 async def create_vacancy_command(message: types.Message, state: FSMContext):
     await message.answer(
-        f'<b>Укажите название для вакансии</b>\n'
-        f'Пример: <i>"Junior Python Developer"</i>', parse_mode='HTML')
+        '<b>Укажите название для вакансии</b>\n'
+        'Пример: <i>Junior Python Developer</i>', parse_mode='HTML')
     await state.set_state(CreateNote.await_title)
 
+
 @vacancy_router.message(CreateNote.await_title)
-async def wait_title_for_vacancy(message: types.Message, state: FSMContext):
-    print(message.text)
+async def process_title(message: types.Message, state: FSMContext):
+    await state.update_data(title=message.text)
+    await message.answer(
+        '<b>Укажите тип занятости</b>\n'
+        'Пример: <i>Full-time, Part-time, Internship</i>', parse_mode='HTML')
+    await state.set_state(CreateNote.await_type_of_employment)
+
+
+@vacancy_router.message(CreateNote.await_type_of_employment)
+async def process_type_of_employment(message: types.Message, state: FSMContext):
+    await state.update_data(type_of_employment=message.text)
+    await message.answer(
+        '<b>Укажите уровень пользователя</b>\n'
+        'Пример: <i>Junior, Middle, Senior</i>', parse_mode='HTML')
+    await state.set_state(CreateNote.await_user_level)
+
+
+@vacancy_router.message(CreateNote.await_user_level)
+async def process_user_level(message: types.Message, state: FSMContext):
+    await state.update_data(user_level=message.text)
+    await message.answer(
+        '<b>Укажите локацию</b>\n'
+        'Пример: <i>Moscow, Remote</i>', parse_mode='HTML')
+    await state.set_state(CreateNote.await_location)
+
+
+@vacancy_router.message(CreateNote.await_location)
+async def process_location(message: types.Message, state: FSMContext):
+    await state.update_data(location=message.text)
+    await message.answer(
+        '<b>Укажите обязанности</b>\n'
+        'Пример: <i>Разработка бэкенда, участие в проектах</i>', parse_mode='HTML')
+    await state.set_state(CreateNote.await_responsibilities)
+
+
+@vacancy_router.message(CreateNote.await_responsibilities)
+async def process_responsibilities(message: types.Message, state: FSMContext):
+    await state.update_data(responsibilities=message.text)
+    await message.answer(
+        '<b>Укажите требования</b>\n'
+        'Пример: <i>Знание Python, опыт работы с Django</i>', parse_mode='HTML')
+    await state.set_state(CreateNote.await_requirements)
+
+
+@vacancy_router.message(CreateNote.await_requirements)
+async def process_requirements(message: types.Message, state: FSMContext):
+    await state.update_data(requirements=message.text)
+    await message.answer(
+        '<b>Укажите, что будет плюсом</b>\n'
+        'Пример: <i>Знание FastAPI, опыт работы с Docker</i>', parse_mode='HTML')
+    await state.set_state(CreateNote.await_mb_plus)
+
+
+@vacancy_router.message(CreateNote.await_mb_plus)
+async def process_mb_plus(message: types.Message, state: FSMContext):
+    await state.update_data(mb_plus=message.text)
+    await message.answer(
+        '<b>Укажите дополнительную информацию</b>', parse_mode='HTML')
+    await state.set_state(CreateNote.await_additionally)
+
+
+@vacancy_router.message(CreateNote.await_additionally)
+async def process_additionally(message: types.Message, state: FSMContext):
+    await state.update_data(additionally=message.text)
+    await message.answer(
+        '<b>Укажите контакты</b>\n'
+        'Пример: <i>telegram: @username, email: example@mail.com</i>', parse_mode='HTML')
+    await state.set_state(CreateNote.await_contacts)
+
+
+@vacancy_router.message(CreateNote.await_contacts)
+async def process_contacts(message: types.Message, state: FSMContext):
+    await state.update_data(contacts=message.text)
+    await message.answer(
+        '<b>Укажите стек технологий</b>\n'
+        'Пример: <i>Python, Django, PostgreSQL</i>', parse_mode='HTML')
+    await state.set_state(CreateNote.await_stack)
+
+
+@vacancy_router.message(CreateNote.await_stack)
+async def process_stack(message: types.Message, state: FSMContext):
+    await state.update_data(stack=message.text)
+    data = await state.get_data()
+    
+    await message.answer(
+        f'<b>Ваша вакансия создана:</b>\n'
+        f'<b>Название:</b> {data["title"]}\n'
+        f'<b>Тип занятости:</b> {data["type_of_employment"]}\n'
+        f'<b>Уровень:</b> {data["user_level"]}\n'
+        f'<b>Локация:</b> {data["location"]}\n'
+        f'<b>Обязанности:</b> {data["responsibilities"]}\n'
+        f'<b>Требования:</b> {data["requirements"]}\n'
+        f'<b>Будет плюсом:</b> {data["mb_plus"]}\n'
+        f'<b>Дополнительно:</b> {data["additionally"]}\n'
+        f'<b>Контакты:</b> {data["contacts"]}\n'
+        f'<b>Стек технологий:</b> {data["stack"]}', parse_mode='HTML')
+    
     await state.clear()
 
-    
-@vacancy_router.message(F.text == ReplyTextCommand.CREATE_RESUME)
-async def create_resume_command(message: types.Message, state: FSMContext):
-    await message.answer('РЕЗЮМЕ')
 
 
-# Сделать что бы 1 резюме можно было вылоить бесплатно, 
-# что бы выложить второе резюме или вакансию надо заплатить
+# # @vacancy_router.message(F.text == ReplyTextCommand.CREATE_RESUME)
+# # async def create_resume_command(message: types.Message, state: FSMContext):
+# #     await message.answer('РЕЗЮМЕ')
+
+
+# # Сделать что бы 1 резюме можно было вылоить бесплатно, 
+# # что бы выложить второе резюме или вакансию надо заплатить
